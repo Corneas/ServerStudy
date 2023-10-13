bdafc418740a4647695246f57232c9626da1c039
Minkyu Kwak <lanilani7661@gmail.com>
2023-10-13 14:34:09 +0900

    데이터베이스 생성 완료. 처음부터 다시 해보도록 하자

--- DataBaseConnectionPractice/App.js
+++ DataBaseConnectionPractice/App.js
@@ -2,16 +2,20 @@ const express = require('express');
 const usersRouter = require('./UsersRouter');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 
-app.use(`/users`, usersRouter);
+app.use(`/Ranks`, usersRouter);
 
-app.get('/', (req, res)=>
-{
-    res.send(`<h2>welcome to server</h2>`);
-});
+// app.get('/', (req, res)=>
+// {
+//     res.send(`<h2>welcome to server</h2>`);
+// });
+
+app.get('/', (req, res) => {
+    
+})
 
 app.listen(port, ()=>
 {
