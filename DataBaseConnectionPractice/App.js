3ff4d0e1ff884b9472e1be29a7f41bba2d280007
Minkyu Kwak <lanilani7661@gmail.com>
2023-10-16 20:26:32 +0900

    post에 중복체크 오류남

--- DataBaseConnectionPractice/App.js
+++ DataBaseConnectionPractice/App.js
@@ -17,19 +17,33 @@ app.get("/", (req, res)=>
 });
 
 app.post('/score', (req, res) =>{
-    var body = req.body;  
-    var sql = "Select * from ranks";
-    conn.query(sql, (err, result) =>{
-        if(err) console.log("query is not excuted : " + err);
-        else{
-            var sql = "insert into ranks(Name, Score) Values(?,?)";
-            var param = [body.Name, body.Score];
-            conn.query(sql, param, (err) =>{
-                if(err) console.log("query is not excuted : " + err);
-                else res.sendStatus(200);
-            });
+    var body = req.body;
+
+    var sql = `select EXISTS (select Name from Ranks where Name = "${body.Name}" limit 1) as success`;
+    console.log(sql);
+    if(conn.query(sql) == 0){
+        console.log("추가");
+        var sql = "Select * from ranks";
+        conn.query(sql, (err, result) =>{
+            if(err) console.log("query is not excuted : " + err);
+            else{
+                var sql = "insert into ranks(Name, Score) Values(?,?)";
+                var param = [body.Name, body.Score];
+                conn.query(sql, param, (err) =>{
+                    if(err) console.log("query is not excuted : " + err);
+                    else res.sendStatus(200);
+                });
+            }
+        });   
+    }
+    else{
+        console.log("업데이트");
+        var sql = "select Score From Ranks Where Name = " + body.Name;
+        if(conn.query(sql) < body.Score){
+            var sql = "Update Ranks Set Score = " + body.Score + " Where Name = " + body.Name;
+            conn.query(sql);
         }
-    });
+    }
 });
 
 app.post('/delete', (req, res) => {
