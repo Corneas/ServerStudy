b84f3ff2048a41af6a7860c780c71dfba05005f9
Minkyu Kwak <lanilani7661@gmail.com>
2023-10-17 09:34:10 +0900

    중복체크 오류 수정

--- DataBaseConnectionPractice/App.js
+++ DataBaseConnectionPractice/App.js
@@ -19,33 +19,40 @@ app.get("/", (req, res)=>
 app.post('/score', (req, res) =>{
     var body = req.body;
 
+    // 중복체크
     var sql = `select EXISTS (select Name from Ranks where Name = "${body.Name}" limit 1) as success`;
-    console.log(sql);
-    if(conn.query(sql) == 0){
-        console.log("추가");
-        var sql = "Select * from ranks";
-        conn.query(sql, (err, result) =>{
-            if(err) console.log("query is not excuted : " + err);
-            else{
-                var sql = "insert into ranks(Name, Score) Values(?,?)";
-                var param = [body.Name, body.Score];
-                conn.query(sql, param, (err) =>{
-                    if(err) console.log("query is not excuted : " + err);
-                    else res.sendStatus(200);
-                });
-            }
-        });   
-    }
-    else{
-        console.log("업데이트");
-        var sql = "select Score From Ranks Where Name = " + body.Name;
-        if(conn.query(sql) < body.Score){
-            var sql = "Update Ranks Set Score = " + body.Score + " Where Name = " + body.Name;
-            conn.query(sql);
+    conn.query(sql, (err, result) => {
+        if(err) console.log("query is not excuted : " + err);
+        
+        // 만약 값이 0개라면
+        if(result[0].success == 0){
+            var sql = "Select * from ranks";
+            // 값 추가
+            conn.query(sql, (err, result) =>{
+                if(err) console.log("query is not excuted : " + err);
+                else{
+                    var sql = "insert into ranks(Name, Score) Values(?,?)";
+                    var param = [body.Name, body.Score];
+                    conn.query(sql, param, (err) =>{
+                        if(err) console.log("query is not excuted : " + err);
+                        else res.sendStatus(200);
+                    });
+                }
+            });   
         }
-    }
+        else{   // 만약 Name이 이미 존재한다면
+            // 업데이트
+            var sql = "select Score From Ranks Where Name = " + body.Name;
+            if(conn.query(sql) < body.Score){
+                var sql = "Update Ranks Set Score = " + body.Score + " Where Name = " + body.Name;
+                conn.query(sql);
+            }
+        }
+        
+    });
 });
 
+// 값 제거
 app.post('/delete', (req, res) => {
     var body = req.body;
     var sql = "Select * from Ranks";
